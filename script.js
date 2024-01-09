871e095304d26f8fe205ea423fad8edfe4bcbc96
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-09 16:03:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function minDate(dates) {
   //write you code here
+	var min = dates.reduce(function (a, b) { return a < b ? a : b; }); 
+	return min;
 }
 
 // Do not change the code
@@ -37,4 +39,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+//alert(minDate(dates));
